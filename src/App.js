75811413a4b317dad57e667270a030d6b55d5ef8
Mehtab's PC <mehtabmultani51@gmail.com>
2021-05-21 18:05:40 +0530

    feature: all components added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+AOS.init();
+
+const App = () => {
+	return (
+		<div>
+			{/* home */}
+			<Home />
+
+			{/* about */}
+			<About />
+
+			{/* services */}
+			<Services />
+
+			{/* pricing */}
+			<Pricing />
+
+			{/* footer */}
+			<Footer />
+		</div>
+	);
+};
 
 export default App;
